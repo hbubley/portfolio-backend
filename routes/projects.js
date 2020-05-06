69adb80a20721096ef92b0efc0ba21d4b73a5224
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-06 15:01:47 -0500

    finished basic framework

---
+++ routes/projects.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+//@route    GET api/projects
+//@desc     Get all projects
+//@access   Public
+
+router.get('/', (req, res) => {
+    res.send('Get all projects');
+}); 
+
+//@route    POST api/projects
+//@desc     Add new project
+//@access   Private
+
+router.post('/', (req, res) => {
+    res.send('Add project ');
+}); 
+
+//@route    PUT api/projects/:id
+//@desc     Update project
+//@access   Private
+
+router.put('/:id', (req, res) => {
+    res.send('Update Project');
+}); 
+
+//@route    DELETE api/projects/:id
+//@desc     Delete project
+//@access   Private
+
+router.delete('/:id', (req, res) => {
+    res.send('Delete Project');
+}); 
+
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => res.json({msg: "Looks like we made it!"}))
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/projects', require('./routes/projects'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
